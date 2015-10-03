a776c02353525e39cb6a27f5618c2407a5238a7a
Justin Kolberg <amd.prophet@gmail.com>
2015-10-02 22:33:18 -0700

    add initial sensu_datacenter resource

---
+++ resources/sensu_datacenter.rb
@@ -0,0 +1,11 @@
+actions :create
+
+attribute :name, :kind_of => String
+attribute :host, :kind_of => String, :default => "127.0.0.1"
+attribute :port, :kind_of => Integer, :default => 4567
+attribute :ssl, :kind_of => Boolean, :default => false
+attribute :insecure, :kind_of => Boolean, :default => false
+attribute :path, :kind_of => String
+attribute :timeout, :kind_of => Integer, :default => 5
+attribute :user, :kind_of => String
+attribute :pass, :kind_of => String
